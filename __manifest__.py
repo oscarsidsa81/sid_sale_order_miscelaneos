411e7366e18ff961ca9d0840dee293110cef82b2
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-11 11:34:49 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'sid_sale_order_miscelaneos',
+    'version': '1.0.1',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Campos en vista tree y form para ayudar a la gestión',
+    'description': 'Campos varios',
+    'author': 'oscarsidsa81',
+    'depends': ['base','sale_management'],
+    'data': [
+        'views/sid_sale_order_miscelaneos.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
